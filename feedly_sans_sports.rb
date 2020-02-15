1567748d9fdcf98d0edcbb54fc1029cf6957318c
Jer Megel <git@jeremiahm.com>
2020-02-15 13:16:38 -0600

    Run every 15 minutes

--- feedly_sans_sports.rb
+++ feedly_sans_sports.rb
@@ -7,38 +7,42 @@ class FeedlyApi
 
   base_uri 'https://feedly.com/v3'
   headers(
-      Authorization: ENV['FEEDLY_API_AUTHORIZATION']
+    Authorization: ENV['FEEDLY_API_AUTHORIZATION']
   )
 end
 
-resp =
-  FeedlyApi.get(
-    '/streams/contents',
-    query: {
-      streamId: ENV['FEEDLY_STREAM'],
-      count: 1000,
-      unreadOnly:  true,
-      ranked: 'oldest',
-      similar: true
-    }
-)
+loop do
+  resp =
+    FeedlyApi.get(
+      '/streams/contents',
+      query: {
+        streamId: ENV['FEEDLY_STREAM'],
+        count: 1000,
+        unreadOnly:  true,
+        ranked: 'oldest',
+        similar: true
+      }
+  )
 
-sports_items =
-  resp['items'].select do |item|
-    item['originId'].include?('/sports/')
-  end
+  sports_items =
+    resp['items'].select do |item|
+      item['originId'].include?('/sports/')
+    end
 
-return unless sports_items.any?
+  return unless sports_items.any?
 
-resp =
-  FeedlyApi.post(
-    '/markers',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: {
-      action: 'markAsRead',
-      type: 'entries',
-      entryIds: sports_items.map { |item| item['id'] }
-    }.to_json
-)
+  resp =
+    FeedlyApi.post(
+      '/markers',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: {
+        action: 'markAsRead',
+        type: 'entries',
+        entryIds: sports_items.map { |item| item['id'] }
+      }.to_json
+  )
+
+  sleep 60 * 15 # 15 minutes
+end
